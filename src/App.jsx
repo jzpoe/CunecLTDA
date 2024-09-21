6d989a497d70bc4c61c7a9ec67c53e04d2fc996f
jzpoe <pmguez1287@gmail.com>
2024-09-21 11:52:13 -0500

    correcion en la exportacion del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 import Asesoramiento from './components/body/asesoramiento/Asesoramiento';
 import Body from './components/body/body/Body';
 import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar'
+
 
 function App() {
   
